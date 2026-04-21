ec3a71799dbff6e74d7526698da84b4b199aa887
Edward He <edward@parallel.ai>
2026-04-20 20:57:28 -0700

    Task run events

--- src/parallel/types/beta/task_run_event.py
+++ src/parallel/types/beta/task_run_event.py
@@ -2,7 +2,10 @@
 
 from .. import task_run_event
 
-__all__ = ["TaskRunEvent"]
+__all__ = ["TaskRunEvent", "Output"]
 
 TaskRunEvent = task_run_event.TaskRunEvent
 """Use parallel.types.task_run.TaskRunEvent instead"""
+
+Output = task_run_event.Output
+"""This is deprecated, use `parallel.types.task_run_event.Output` instead"""
