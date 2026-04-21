0b84d48ff9980518d71af88917f99746e74b4a33
Edward He <edward@parallel.ai>
2026-04-20 20:43:08 -0700

    lint fix

--- src/parallel/types/beta/__init__.py
+++ src/parallel/types/beta/__init__.py
@@ -28,23 +28,50 @@ from .parallel_beta_param import ParallelBetaParam as ParallelBetaParam
 from .beta_run_input_param import BetaRunInputParam as BetaRunInputParam
 from .beta_task_run_result import BetaTaskRunResult as BetaTaskRunResult
 from .findall_enrich_input import FindAllEnrichInput as FindAllEnrichInput, FindallEnrichInput as FindallEnrichInput
-from .findall_create_params import FindAllCreateParams as FindAllCreateParams, FindallCreateParams as FindallCreateParams
-from .findall_enrich_params import FindAllEnrichParams as FindAllEnrichParams, FindallEnrichParams as FindallEnrichParams
-from .findall_events_params import FindAllEventsParams as FindAllEventsParams, FindallEventsParams as FindallEventsParams
-from .findall_extend_params import FindAllExtendParams as FindAllExtendParams, FindallExtendParams as FindallExtendParams
-from .findall_ingest_params import FindAllIngestParams as FindAllIngestParams, FindallIngestParams as FindallIngestParams
+from .findall_create_params import (
+    FindAllCreateParams as FindAllCreateParams,
+    FindallCreateParams as FindallCreateParams,
+)
+from .findall_enrich_params import (
+    FindAllEnrichParams as FindAllEnrichParams,
+    FindallEnrichParams as FindallEnrichParams,
+)
+from .findall_events_params import (
+    FindAllEventsParams as FindAllEventsParams,
+    FindallEventsParams as FindallEventsParams,
+)
+from .findall_extend_params import (
+    FindAllExtendParams as FindAllExtendParams,
+    FindallExtendParams as FindallExtendParams,
+)
+from .findall_ingest_params import (
+    FindAllIngestParams as FindAllIngestParams,
+    FindallIngestParams as FindallIngestParams,
+)
 from .excerpt_settings_param import ExcerptSettingsParam as ExcerptSettingsParam
 from .task_run_create_params import TaskRunCreateParams as TaskRunCreateParams
 from .task_run_result_params import TaskRunResultParams as TaskRunResultParams
-from .findall_events_response import FindAllEventsResponse as FindAllEventsResponse, FindallEventsResponse as FindallEventsResponse
+from .findall_events_response import (
+    FindAllEventsResponse as FindAllEventsResponse,
+    FindallEventsResponse as FindallEventsResponse,
+)
 from .task_group_run_response import TaskGroupRunResponse as TaskGroupRunResponse
-from .findall_run_status_event import FindAllRunStatusEvent as FindAllRunStatusEvent, FindallRunStatusEvent as FindallRunStatusEvent
+from .findall_run_status_event import (
+    FindAllRunStatusEvent as FindAllRunStatusEvent,
+    FindallRunStatusEvent as FindallRunStatusEvent,
+)
 from .task_group_create_params import TaskGroupCreateParams as TaskGroupCreateParams
 from .task_group_events_params import TaskGroupEventsParams as TaskGroupEventsParams
 from .task_run_events_response import TaskRunEventsResponse as TaskRunEventsResponse
 from .task_group_add_runs_params import TaskGroupAddRunsParams as TaskGroupAddRunsParams
 from .task_group_events_response import TaskGroupEventsResponse as TaskGroupEventsResponse
 from .task_group_get_runs_params import TaskGroupGetRunsParams as TaskGroupGetRunsParams
-from .findall_schema_updated_event import FindAllSchemaUpdatedEvent as FindAllSchemaUpdatedEvent, FindallSchemaUpdatedEvent as FindallSchemaUpdatedEvent
+from .findall_schema_updated_event import (
+    FindAllSchemaUpdatedEvent as FindAllSchemaUpdatedEvent,
+    FindallSchemaUpdatedEvent as FindallSchemaUpdatedEvent,
+)
 from .task_group_get_runs_response import TaskGroupGetRunsResponse as TaskGroupGetRunsResponse
-from .findall_candidate_match_status_event import FindAllCandidateMatchStatusEvent as FindAllCandidateMatchStatusEvent, FindallCandidateMatchStatusEvent as FindallCandidateMatchStatusEvent
+from .findall_candidate_match_status_event import (
+    FindAllCandidateMatchStatusEvent as FindAllCandidateMatchStatusEvent,
+    FindallCandidateMatchStatusEvent as FindallCandidateMatchStatusEvent,
+)
